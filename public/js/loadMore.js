e3af7dfd79599eb035128040a5f51adca1b8c362
Niki <jdeejat@gmail.com>
2022-12-09 22:28:52 +0100

    added toast

--- public/js/loadMore.js
+++ public/js/loadMore.js
@@ -7,5 +7,20 @@ export const loadMore = async (page = 0, limit = 12) => {
     method: 'GET',
   });
 
+  Toastify({
+    text: 'This is a toast',
+    duration: 3000,
+    destination: 'https://github.com/apvarun/toastify-js',
+    newWindow: true,
+    close: true,
+    gravity: 'bottom', // `top` or `bottom`
+    position: 'center', // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: 'linear-gradient(to right, #00b09b, #96c93d)',
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
+
   return await request.text();
 };
